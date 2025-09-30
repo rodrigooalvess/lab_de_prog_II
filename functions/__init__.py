bb3860435203ca2e191e6c8b0327b0a4ea213933
Rodrigo Antonio <rodrigoantonioalves2006@hotmail.com>
2025-09-30 19:26:21 -0300

    Update __init__.py
    import das funções de conversão de moedas

--- functions/__init__.py
+++ functions/__init__.py
@@ -1,3 +1,4 @@
 from .conversor import celsius_fahrenheit, fahrenheit_celsius
 from .verificador import verificar_par_impar
 from .notas import media, maior, menor
+from .conversor_moedas import real_dolar, dolar_real
