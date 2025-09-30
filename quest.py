94c8931001c8611815c986513f64dcf38fb30d39
Rodrigo Antonio <rodrigoantonioalves2006@hotmail.com>
2025-09-30 10:58:50 -0300

    Update quest.py
    codigo da questao 1

---
+++ quest.py
@@ -0,0 +1,18 @@
+from functions import *
+
+def questao_1():
+    print('Conversor de Temperaturas')
+    print('1 - Celsius p/ Fahrenheit')
+    print('2 - Fahrenheit p/ Celsius')
+
+    opc = int(input('Digite a opção que deseja: '))
+
+    if opc == 1:
+        celsius = float(input('Digite a temperatura que deseja converter: '))
+        print(f'Temperatura convertida: {celsius_fahrenheit(celsius):.1f}F')
+    elif opc == 2:
+        fahrenheit = float(input('Digite a temperatura que deseja converter: '))
+        print(f'Temperatura convertida: {fahrenheit_celsius(fahrenheit):.1f}C')
+        
+    else:
+        print('Opção Inválida')
