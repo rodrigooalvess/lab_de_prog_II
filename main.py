742fc76834cb33cfbfd862053fb9495350f68a13
Rodrigo Antonio <rodrigoantonioalves2006@hotmail.com>
2025-10-01 09:36:15 -0300

    Update main.py
    adicionando while true, e criando opção de sair do código

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from quest import *
 
-questao = int(input('Digite a Questão (1 a 6): '))
+while True:
 
-if questao == 1:
-    questao_1()
-if questao == 2:
-    questao_2()
-if questao == 3:
-    questao_3()
-if questao == 4:
-    questao_4()
-if questao == 5:
-    questao_5()
-if questao == 6:
-    questao_6()
-else:
-    print('OPÇÃO INVÁLIDA')
+    questao = int(input('Digite a Questão (1 a 6) ou Digite 0 p/ Sair: '))
+
+    if questao == 0:
+        print('SAINDO DO PROGRAMA...')
+        break
+    elif questao == 1:
+        questao_1()
+    elif questao == 2:
+        questao_2()
+    elif questao == 3:
+        questao_3()
+    elif questao == 4:
+        questao_4()
+    elif questao == 5:
+        questao_5()
+    elif questao == 6:
+        questao_6()
+    else:
+        print('OPÇÃO INVÁLIDA')
